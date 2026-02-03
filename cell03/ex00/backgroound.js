558c17a7a8d37c8d2d93c861f40596c04e543cda
Chanipha-glitch <67125005@kmitl.ac.th>
2026-02-04 00:21:01 +0700

    updated cell03 ex00 backgroound

--- cell03/ex00/backgroound.js
+++
@@ -1,9 +0,0 @@
-<script>
-    function changeColor() {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-
-        document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    }
-</script>
